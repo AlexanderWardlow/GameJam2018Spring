2d7ad248bbba871fe8171c4a7ed5267057d93ca8
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-09 01:00:50 -0500

    Added Shrink/Grow Platforms, called reState
    These objects will transition between an inial state, (set by user) to a final state (also set by user). It will currently transition via size and position and active (visible) state. The transition is directly proportional to the distance to the player as difined by the (max Dist, minDist public floats).

---
+++ PracticeForGameJam2018/Assets/Scripts/changeStateBasedOnPlayerDist.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeStateBasedOnPlayerDist : MonoBehaviour {
+
+	public Transform Player;
+	public Transform initialState;
+	public Transform finalState;
+	private BoxCollider bxCol;
+	private MeshRenderer mRend;
+	public float maxDist;
+	public float minDist;
+	public float distanceToPlayer;
+	public bool finalActive;
+	public bool initialActive;
+
+	void Start() {
+		bxCol = transform.GetComponent<BoxCollider> ();
+		mRend = transform.GetComponent<MeshRenderer> ();
+		setInitialState ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		distanceToPlayer = Vector3.Distance (finalState.position,Player.position);
+		checkPlayerDist ();
+	}
+
+	void checkPlayerDist(){
+		if (distanceToPlayer <= maxDist) {
+			if (distanceToPlayer >= minDist) {
+				mRend.enabled = (true);
+				bxCol.enabled = (true);
+				reState ();
+			}
+			else {
+				setFinalState ();
+			}
+		}
+		else {
+			setInitialState ();
+		}
+	}
+
+	void reState() {
+		float progression = distanceToPlayer - minDist;
+		float total = maxDist - minDist;
+		float multiplier = (total - progression) / (total);
+
+		reSize(multiplier);
+		rePosition (multiplier);
+		//reRotation (multiplier);
+	}
+
+	void reSize(float multiplier) {
+		Vector3 scaleDiff = finalState.localScale - initialState.localScale;
+		Vector3 scaleAdjust = scaleDiff * multiplier;
+		transform.localScale = initialState.localScale + scaleAdjust;
+	}
+
+	void rePosition(float multiplier) {
+		Vector3 posDiff = finalState.position - initialState.position;
+		Vector3 positionAdjust = posDiff * multiplier;
+		transform.position = initialState.position + positionAdjust;
+	}
+	/* Rotation does not work this way, may need some work
+	void reRotation(float multiplier) {
+		Vector3 rotDiff = finalState.rotation - initialState.rotation;
+		Vector3 rotationAdjust = rotDiff * multiplier;
+		transform.rotation = Quaternion.Euler(initialState.rotation + rotationAdjust);
+	}
+*/
+	void setFinalState() {
+		mRend.enabled = (finalActive);
+		bxCol.enabled = (finalActive);
+		transform.localScale = finalState.localScale;
+		transform.localRotation = finalState.localRotation;
+		//transform.position = finalState.position;
+	}
+	void setInitialState() {
+		mRend.enabled = (initialActive);
+		bxCol.enabled = (initialActive);
+		transform.localScale = initialState.localScale;
+		transform.position = initialState.position;
+		//transform.localRotation = initialState.localRotation;
+	}
+}
---
+++ PracticeForGameJam2018/Assets/Scripts/reStateDistanceBased.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeBasedOnDistance : MonoBehaviour {
+
+	public Transform Player;
+	public Transform initialState;
+	public Transform finalState;
+	public float maxDist;
+	public float minDist;
+	private float distanceToPlayer;
+	public bool finalActive;
+	public bool initialActive;
+
+	// Update is called once per frame
+	void Update () {
+		distanceToPlayer = Vector3.Distance (finalState.position,Player.position);
+	}
+
+	void checkPlayerDist(){
+		if (distanceToPlayer <= maxDist) {
+			if (distanceToPlayer >= minDist) {
+				transform.gameObject.SetActive (false);
+				reState ();
+			}
+			else {
+				setFinalState ();
+			}
+		}
+		else {
+			setInitialState ();
+		}
+	}
+
+	void reState() {
+		float progression = distanceToPlayer - minDist;
+		float total = maxDist - minDist;
+		float multiplier = (total - progression) / (total);
+
+		Vector3 scaleDiff = finalState.localScale - initialState.localScale;
+		Vector3 posDiff = finalState.position - initialState.position;
+		Vector3 scaleAdjust = scaleDiff * multiplier;
+		Vector2 positionAdjust = posDiff * multiplier;
+
+		reSize(scaleAdjust);
+		rePosition (positionAdjust);
+	}
+
+	void reSize(Vector3 scaleAdjust) {
+		transform.localScale = initialState.localScale + scaleAdjust;
+	}
+
+	void rePosition(Vector3 positionAdjust) {
+		transform.position = initialState.position + positionAdjust;
+	}
+
+	void setFinalState() {
+		transform.gameObject.SetActive (finalActive);
+		transform.localScale = finalState.localScale;
+		transform.position = finalState.position;
+	}
+	void setInitialState() {
+		transform.gameObject.SetActive (initialActive);	
+		transform.localScale = initialState.localScale;
+		transform.position = initialState.position;
+	}
+}
